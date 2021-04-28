f902ee840612d563239dea73d7c3aa0fd97ab1d9
TetaliVarshitha <69201555+TetaliVarshitha@users.noreply.github.com>
2021-04-28 15:28:09 +0530

    Add files via upload

---
+++ 18WH1A0570_Program1.py.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+probAbsentFriday = 0.03
+probFriday = 0.2
+
+
+# In[6]:
+
+
+bayesResult = (probAbsentFriday/probFriday)
+print(bayesResult * 100)
+
+
+# In[ ]:
+
+
+
+
